64b7065896825efaec757468bf3cf03f09c9fac7
IaacobH <iaacobh@gmail.com>
2026-08-03 14:36:42 -0300

    fix: correccion de errores

--- src/main/java/repository/OfferedServiceRepository.java
+++ src/main/java/repository/OfferedServiceRepository.java
@@ -22,7 +22,7 @@ public class OfferedServiceRepository {
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             ps.setString(1, offeredService.getServiceName());
-            ps.setDouble(1, offeredService.getPrice());
+            ps.setDouble(2, offeredService.getPrice());
 
             ps.executeUpdate();
             ResultSet key = ps.getGeneratedKeys();
