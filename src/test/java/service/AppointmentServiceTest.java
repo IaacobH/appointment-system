d183f907625c3ca2e27755c218cc46571bd53822
IaacobH <iaacobh@gmail.com>
2026-08-18 11:42:00 -0300

    feat: some tests on appointmentServiceTest

--- src/test/java/service/AppointmentServiceTest.java
+++ src/test/java/service/AppointmentServiceTest.java
@@ -1,65 +1,106 @@
 package service;
 
-import model.Client;
+import exception.EntityNotFoundException;
+import model.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import repository.AppointmentRepository;
-import repository.ClientRepository;
-import repository.OfferedServiceRepository;
-import repository.ProfessionalRepository;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class AppointmentServiceTest {
 
-    @Mock
-    AppointmentRepository appointmentRepository;
+    @Mock AppointmentRepository appointmentRepository;
+    @Mock ClientService clientService;
+    @Mock ProfessionalService professionalService;
+    @Mock OfferedServiceService offeredServiceService;
+
 
     @InjectMocks
     AppointmentService appointmentService;
 
     @Test
-    void shouldCreateAppointmentAndWithId() {
-        var clientRepository = new ClientRepository();
-        var clientService = new ClientService(clientRepository);
-        var offeredServiceRepository = new OfferedServiceRepository();
-        var offeredServiceService = new OfferedServiceService(offeredServiceRepository);
-        var professionalRepository = new ProfessionalRepository();
-        var professionalService = new ProfessionalService(professionalRepository);
-        var appointmentRepository = new AppointmentRepository();
-        var appointmentService = new AppointmentService(appointmentRepository, clientService,
-                professionalService, offeredServiceService);
+    void shouldCreateAppointment() {
+        var client = new Client(1, "Iaacob", "Hambra", "iaacob@email.com");
+        var professional = new Professional(1, "name1", "surname1", "email1", "speciality1");
+        var offeredService = new OfferedService(1, "name1", 40);
+        var dateTime = LocalDateTime.of(2026, 12, 25, 18, 30);
 
-        Client client1 = clientService.register("Iaacob", "Hambra", "iaacob@email.com");
-        var offeredService1 = offeredServiceService.register("name1", 40);
-        var professional1 = professionalService.register("name1", "surname1", "email1",
-                "speciality1");
+        var appointment = appointmentService.createAppointment(offeredService, professional, client, dateTime);
 
-        var datetime1 = LocalDateTime.of(2026, 12, 25, 18, 30);
-        var datetime2 = LocalDateTime.of(2030,10,10,10,10);
+        assertEquals(offeredService, appointment.getOfferedService());
+        assertEquals(professional, appointment.getProfessional());
+        assertEquals(client, appointment.getClient());
+        assertEquals(dateTime, appointment.getDateTime());
+        verify(appointmentRepository).save(appointment);
+    }
 
-        var appointment1 = appointmentService.createAppointment(
-                offeredService1,
-                professional1,
-                client1,
-                LocalDateTime.of(2026, 12, 25, 18, 30));
+    @Test
+    void shouldCreateAppointmentWithId_whenNoConflicts() {
+        var client = new Client(1, "Iaacob", "Hambra", "iaacob@email.com");
+        var professional = new Professional(1, "name1", "surname1", "email1", "speciality1");
+        var offeredService = new OfferedService(1, "name1", 40);
+        var dateTime = LocalDateTime.of(2030, 10, 10, 10, 10);
 
-        assertEquals(1, appointment1.getId());
-        assertEquals(offeredService1, appointment1.getOfferedService());
-        assertEquals(professional1, appointment1.getProfessional());
-        assertEquals(client1, appointment1.getClient());
-        assertEquals(datetime1, appointment1.getDateTime());
+        when(clientService.findById(1)).thenReturn(client);
+        when(professionalService.findById(1)).thenReturn(professional);
+        when(offeredServiceService.findById(1)).thenReturn(offeredService);
+        when(appointmentRepository.existsByProfessionalAndDateTime(professional, dateTime)).thenReturn(false);
+        when(appointmentRepository.existsByClientAndDateTime(client, dateTime)).thenReturn(false);
 
-        var appointment2 = appointmentService.createAppointmentWithId(1,1,1,datetime2);
-        assertEquals(2, appointment2.getId());
-        assertEquals(offeredService1, appointment2.getOfferedService());
-        assertEquals(professional1, appointment2.getProfessional());
-        assertEquals(client1, appointment2.getClient());
-        assertEquals(datetime2, appointment2.getDateTime());
+        var appointment = appointmentService.createAppointmentWithId(1, 1, 1, dateTime);
+
+        assertEquals(client, appointment.getClient());
+        assertEquals(professional, appointment.getProfessional());
+        assertEquals(offeredService, appointment.getOfferedService());
+        verify(appointmentRepository).save(appointment);
+    }
+
+    @Test
+    void shouldFindAll(){
+        var client = new Client(1, "Iaacob", "Hambra", "iaacob@email.com");
+        var professional = new Professional(1, "name1", "surname1", "email1", "speciality1");
+        var offeredService = new OfferedService(1, "name1", 40);
+        var dateTime = LocalDateTime.of(2030, 10, 10, 10, 10);
+        var a1 = new Appointment(offeredService, professional, client, dateTime);
+        var a2 = new Appointment(offeredService, professional, client, dateTime);
+
+        when(appointmentRepository.findAll()).thenReturn(List.of(a1,a2));
+        var appointments = appointmentService.findAll();
+
+        assertEquals(2, appointments.size());
+        assertTrue(appointments.contains(a1));
+        assertTrue(appointments.contains(a2));
+
+    }
+
+    @Test
+    void findById(){
+        var client = new Client(1, "Iaacob", "Hambra", "iaacob@email.com");
+        var professional = new Professional(1, "name1", "surname1", "email1", "speciality1");
+        var offeredService = new OfferedService(1, "name1", 40);
+        var dateTime = LocalDateTime.of(2030, 10, 10, 10, 10);
+        var a1 = new Appointment(1,offeredService, professional, client, dateTime, AppointmentStatus.SCHEDULED);
+
+        when(appointmentRepository.findById(1)).thenReturn(Optional.of(a1));
+        var appointmentFound = appointmentService.findById(1);
+
+        assertEquals(a1, appointmentFound);
+    }
+
+    @Test
+    void shouldThrowWhenAppointmentDoesntExists(){
+        when(appointmentRepository.findById(999)).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class,
+                () -> appointmentService.findById(999));
 
     }
 
